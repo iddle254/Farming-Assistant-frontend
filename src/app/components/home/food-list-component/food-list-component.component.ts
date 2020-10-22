cd8059ebb348e1efcb0ba91d9d59ddbe0a7abbc9
Charles <charlesiddle99@gmail.com>
2020-10-22 23:17:47 +0300

    added the foodlist component

---
+++ src/app/components/home/food-list-component/food-list-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-list-component',
+  templateUrl: './food-list-component.component.html',
+  styleUrls: ['./food-list-component.component.css']
+})
+export class FoodListComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
