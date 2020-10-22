6d45a9f40cebf3f58bd951f1b0770d834110a524
Charles <charlesiddle99@gmail.com>
2020-10-22 23:15:32 +0300

    added the food item component

---
+++ src/app/components/home/food-item-component/food-item-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-item-component',
+  templateUrl: './food-item-component.component.html',
+  styleUrls: ['./food-item-component.component.css']
+})
+export class FoodItemComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
