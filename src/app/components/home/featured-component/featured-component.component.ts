53f8d7b9981d238742ace15713de3f2ea7d9d7af
Charles <charlesiddle99@gmail.com>
2020-10-22 23:11:00 +0300

    added the featured component

---
+++ src/app/components/home/featured-component/featured-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-component',
+  templateUrl: './featured-component.component.html',
+  styleUrls: ['./featured-component.component.css']
+})
+export class FeaturedComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
