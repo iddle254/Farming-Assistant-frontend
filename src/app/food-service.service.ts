599d4b1087899b2c3f6d48b032baa8ba87f4848d
Charles <charlesiddle99@gmail.com>
2020-10-23 08:47:48 +0300

    added mock foods data

--- src/app/food-service.service.ts
+++ src/app/food-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Food } from './components/home/food';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodServiceService {
+  private _foodList = './components/home/foods.json';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getFoods(): Observable<Food[]> {
+    return this.httpClient.get(this._foodList).map((food) => <Food>food.json());
+  }
 }
